60babd615c5584c7b4d27cc6e2b217df0d990754
levintoo <tooklevin@gmail.com>
2022-07-21 11:49:29 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const count = ref(0)
+const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
+
+onMounted(() => {
+  alert("mounted");
+});
+</script>
+
+<template>
+  <div>
+    <h1 @click="count++">Hello from vue {{ count }}</h1>
+    <li v-for="item in items">
+      {{ item.message }}
+    </li>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  cursor: pointer;
+}
+</style>
